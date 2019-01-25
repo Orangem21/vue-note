0b4e3337b1df71f6a0c577ff0679d055414f11dc
OrangeM21 <orangemtony@gmail.com>
2019-01-25 13:53:43 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <TodoInput></TodoInput>
+      <TodoOutput></TodoOutput>
+    </div>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoOutput from "./components/TodoOutput.vue";
+import TodoInput from "./components/TodoInput.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    TodoOutput,
+    TodoInput
   }
-}
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: Arial, 宋体;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  background-color: #edf5f8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.container {
+  width: 60%;
+  height: 60%;
+  padding: 30px;
+  background-color: #fff;
+  box-shadow: 0 10px 12px #bbb;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
+
+
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <input
+    class="todo-input"
+    placeholder="从这里添加一个新的待办事项"
+    v-model.trim="todoContent"
+    @keyup.enter="add"
+  >
+</template>
+
+<script>
+import { ADD_TODO } from "../state/state";
+export default {
+  name: "TodoInput",
+  data() {
+    return {
+      todoContent: ""
+    };
+  },
+  methods: {
+    add() {
+      if (this.todoContent === "") {
+        return false;
+      }
+      //存储在Store中
+      this.$store.commit(ADD_TODO, this.todoContent);
+      //清空Input中的值
+      this.todoContent = "";
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.todo-input {
+  width: 60%;
+  height: 35px;
+  padding: 0 10px;
+  font-size: 14px;
+  border: 3px solid #c4e3ef;
+  border-radius: 50px;
+  outline: none;
+}
+</style>
